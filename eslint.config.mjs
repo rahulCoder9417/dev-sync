3b0b3c1631043fa42ef8b42a2892577b4bd7f13f
Rahul Kumar <rahulkum94l94@gmail.com>
2025-07-09 11:34:20 +0530

    f

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,4 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
-module.exports = {
-  ignorePatterns: ["lib/generated/**"],
-};
 export default eslintConfig;
