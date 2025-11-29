6e5bd2af0acf50ca0b3298deb92b32aa44a59fb6
Rahul Kumar <rahulkum94l94@gmail.com>
2025-11-29 19:24:50 +0530

    a

--- renderBeckend/lib/db/db.ts
+++ renderBeckend/lib/db/db.ts
@@ -1,3 +1,8 @@
 import { PrismaClient } from "@prisma/client";
-
-export const db = new PrismaClient();
+export const db = new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.POOL_DATABASE_URL || process.env.DATABASE_URL
+      }
+    }
+  });
