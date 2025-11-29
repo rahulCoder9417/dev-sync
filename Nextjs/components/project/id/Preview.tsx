511cc2baed37ac305586bc1f94b5a3e68f336bfd
Rahul Kumar <rahulkum94l94@gmail.com>
2025-11-29 16:10:14 +0530

    fixed imports render

--- Nextjs/components/project/id/Preview.tsx
+++ Nextjs/components/project/id/Preview.tsx
@@ -1,8 +1,10 @@
+import useTerminal from '@/customHooks/useTerminal'
 import React from 'react'
 
 const Preview = () => {
+  useTerminal()
   return (
-    <div>
+    <div className='w-full h-full bg-secondary'>
       
     </div>
   )
