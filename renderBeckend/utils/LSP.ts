511f46d592713d2d9db7fabecd3adb34949763b8
Rahul <rahulkum94l94@gmail.com>
2026-03-02 19:33:43 +0530

    feat:now stdout lsp

--- renderBeckend/utils/LSP.ts
+++ renderBeckend/utils/LSP.ts
@@ -1,34 +1,60 @@
-import { spawn } from "child_process";
+import { spawn, ChildProcess } from "child_process";
 import path from "path";
 
 export class LSP {
   private projectRoot: string;
+  private lsp: ChildProcess;
+  private rootUri: string;
   constructor(projectRoot: string) {
     this.projectRoot = projectRoot;
+    this.rootUri = "file:///" + this.projectRoot.replace(/\\/g, "/");
     this.start();
   }
-  private start() {
-    const lspPath = path.join(
-    process.cwd(),
-    "node_modules",
-    "typescript-language-server",
-    "lib",
-    "cli.mjs"
-  );
 
-  const lsp = spawn(
-    process.execPath, 
-    [lspPath, "--stdio"], 
-    {
+  private start() {
+    const isWindows = process.platform === "win32";
+
+    const command = isWindows ? "npx" : "typescript-language-server";
+
+    const args = isWindows
+      ? ["typescript-language-server", "--stdio"]
+      : ["--stdio"];
+
+    this.lsp = spawn(command, args, {
       cwd: this.projectRoot,
-    },
-  );
-    console.log("LSP started for ", this.projectRoot.split(path.sep).pop());
-    lsp.stderr.on("data", (data) => {
+      shell: isWindows,
+    });
+
+    this.send({
+      jsonrpc: "2.0",
+      id: 1,
+      method: "initialize",
+      params: {
+        processId: process.pid,
+        rootUri: this.rootUri,
+        capabilities: {},
+      },
+    });
+
+    this.lsp.stdout.on("data", (chunk) => {
+      console.log("RAW:", chunk.toString());
+    });
+
+    this.lsp.stderr.on("data", (data) => {
       console.error("LSP stderr:", data.toString());
     });
-    lsp.on("exit", (code) => {
+
+    this.lsp.on("exit", (code) => {
       console.log("LSP exited with code:", code);
     });
+  }
+
+  private send(message: Record<string, unknown>) {
+    const json = JSON.stringify(message);
+    const contentLength = Buffer.byteLength(json, "utf8");
+
+    const payload = `Content-Length: ${contentLength}\r\n` + `\r\n` + json;
+
+    this.lsp.stdin.write(payload);
   }
 }
